d9641e4630978032409787f4446af00694040cfb
Celso Aleixo celso.aleixo@uello.com.br <celso.aleixo@uello.com.br>
2023-12-01 20:07:41 -0300

    (feat) extends animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
+      keyframes: {
+        floatUp: {
+          "0%": { transform: "translatey(0px)" },
+          "50%": { transform: "translatey(-8px)" },
+          "100%": { transform: "translate(0px)" },
+        }
+      },
+      animation: {
+        floatUp: "floatUp 2s infinite"
+      },
       colors: {
         primaria: "#FF00FF"
       }
